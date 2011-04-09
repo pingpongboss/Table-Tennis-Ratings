e9097cc3357c525b6d41b038bf8e9107eccb6163
Mark Wei <markwei@gmail.com>
2011-04-09 03:00:45 -0700

    extract colors

--- gen/wei/mark/tabletennis/R.java
+++ gen/wei/mark/tabletennis/R.java
@@ -10,6 +10,12 @@ package wei.mark.tabletennis;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int button_text=0x7f040003;
+        public static final int dark_gray=0x7f040000;
+        public static final int edittext_hint=0x7f040002;
+        public static final int edittext_text=0x7f040001;
+    }
     public static final class drawable {
         public static final int action_bar=0x7f020000;
         public static final int empty_content=0x7f020001;
@@ -43,22 +49,22 @@ public final class R {
         public static final int white_selector=0x7f02001d;
     }
     public static final class id {
-        public static final int action_bar=0x7f060006;
-        public static final int content=0x7f060003;
-        public static final int content_rc=0x7f060005;
-        public static final int content_usatt=0x7f060004;
-        public static final int emptyListText=0x7f060008;
-        public static final int logo=0x7f060000;
-        public static final int message=0x7f06000d;
-        public static final int name=0x7f06000e;
-        public static final int progress=0x7f060007;
-        public static final int provider_logo=0x7f060001;
-        public static final int rating=0x7f06000f;
-        public static final int retry=0x7f060009;
-        public static final int searchButton=0x7f06000c;
-        public static final int searchEditText=0x7f06000b;
-        public static final int searchLinearLayout=0x7f06000a;
-        public static final int title=0x7f060002;
+        public static final int action_bar=0x7f070006;
+        public static final int content=0x7f070003;
+        public static final int content_rc=0x7f070005;
+        public static final int content_usatt=0x7f070004;
+        public static final int emptyListText=0x7f070008;
+        public static final int logo=0x7f070000;
+        public static final int message=0x7f07000d;
+        public static final int name=0x7f07000e;
+        public static final int progress=0x7f070007;
+        public static final int provider_logo=0x7f070001;
+        public static final int rating=0x7f07000f;
+        public static final int retry=0x7f070009;
+        public static final int searchButton=0x7f07000c;
+        public static final int searchEditText=0x7f07000b;
+        public static final int searchLinearLayout=0x7f07000a;
+        public static final int title=0x7f070002;
     }
     public static final class layout {
         public static final int action_bar=0x7f030000;
@@ -72,34 +78,34 @@ public final class R {
         public static final int item_player_search=0x7f030008;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
     }
     public static final class style {
-        public static final int ActionBar=0x7f05000d;
-        public static final int ActionBarItem=0x7f05000e;
-        public static final int ActionBarItem_Logo=0x7f05000f;
-        public static final int ActionBarItem_ProviderLogo=0x7f050011;
-        public static final int ActionBarItem_ProviderLogoLayout=0x7f050010;
-        public static final int ActionBarItem_Text=0x7f050012;
-        public static final int Button=0x7f050003;
-        public static final int Button_Right=0x7f050005;
-        public static final int Button_Right_Search=0x7f050007;
-        public static final int EditText=0x7f050002;
-        public static final int EditText_Left=0x7f050004;
-        public static final int EditText_Left_Search=0x7f050006;
-        public static final int EmptyListIndicator=0x7f050016;
-        public static final int EmptyListLayout=0x7f050015;
-        public static final int HorizontalSpacer=0x7f050017;
-        public static final int ListView=0x7f050013;
-        public static final int ListViewItem=0x7f050014;
-        public static final int RootLayout=0x7f050008;
-        public static final int RootLayout_List=0x7f05000b;
-        public static final int RootLayout_List_Landscape=0x7f05000c;
-        public static final int RootLayout_Search=0x7f050009;
-        public static final int RootLayout_Search_Landscape=0x7f05000a;
-        public static final int Theme_DarkBlue=0x7f050001;
-        public static final int Theme_Dialog_Translucent=0x7f050000;
-        public static final int VerticalSpacer=0x7f050018;
+        public static final int ActionBar=0x7f06000d;
+        public static final int ActionBarItem=0x7f06000e;
+        public static final int ActionBarItem_Logo=0x7f06000f;
+        public static final int ActionBarItem_ProviderLogo=0x7f060011;
+        public static final int ActionBarItem_ProviderLogoLayout=0x7f060010;
+        public static final int ActionBarItem_Text=0x7f060012;
+        public static final int Button=0x7f060003;
+        public static final int Button_Right=0x7f060005;
+        public static final int Button_Right_Search=0x7f060007;
+        public static final int EditText=0x7f060002;
+        public static final int EditText_Left=0x7f060004;
+        public static final int EditText_Left_Search=0x7f060006;
+        public static final int EmptyListIndicator=0x7f060016;
+        public static final int EmptyListLayout=0x7f060015;
+        public static final int HorizontalSpacer=0x7f060017;
+        public static final int ListView=0x7f060013;
+        public static final int ListViewItem=0x7f060014;
+        public static final int RootLayout=0x7f060008;
+        public static final int RootLayout_List=0x7f06000b;
+        public static final int RootLayout_List_Landscape=0x7f06000c;
+        public static final int RootLayout_Search=0x7f060009;
+        public static final int RootLayout_Search_Landscape=0x7f06000a;
+        public static final int Theme_DarkBlue=0x7f060001;
+        public static final int Theme_Dialog_Translucent=0x7f060000;
+        public static final int VerticalSpacer=0x7f060018;
     }
 }
