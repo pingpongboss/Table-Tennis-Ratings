fed02bf4f9663392ececcce3ac1ddbbbf681f455
Mark Wei <markwei@gmail.com>
2011-04-14 17:56:12 -0700

    new server model

--- src/wei/mark/tabletennis/model/PlayerModel.java
+++ src/wei/mark/tabletennis/model/PlayerModel.java
@@ -1,18 +1,24 @@
 package wei.mark.tabletennis.model;
 
+import java.util.Date;
+
 import android.os.Parcel;
 import android.os.Parcelable;
 
 public class PlayerModel implements Parcelable {
-	private String mProvider;
-	private String mRating;
-	private String mName;
-	private String mId;
-	private String[] mClubs;
-	private String mState;
-	private String mCountry;
-	private String mLastPlayed;
-	private String mExpires;
+	Long key;
+
+	String provider;
+	String id;
+
+	String rating;
+	String name;
+	String[] clubs;
+	String state;
+	String country;
+	String lastPlayed;
+	String expires;
+	Date refreshed;
 
 	public PlayerModel() {
 	}
@@ -28,136 +34,160 @@ public class PlayerModel implements Parcelable {
 
 	@Override
 	public void writeToParcel(Parcel dest, int flags) {
-		dest.writeString(mProvider);
-		dest.writeString(mRating);
-		dest.writeString(mName);
-		dest.writeString(mId);
-		dest.writeStringArray(mClubs);
-		dest.writeString(mState);
-		dest.writeString(mCountry);
-		dest.writeString(mLastPlayed);
-		dest.writeString(mExpires);
+		dest.writeLong(key);
+		dest.writeString(provider);
+		dest.writeString(id);
+		dest.writeString(rating);
+		dest.writeString(name);
+		dest.writeStringArray(clubs);
+		dest.writeString(state);
+		dest.writeString(country);
+		dest.writeString(lastPlayed);
+		dest.writeString(expires);
+		dest.writeSerializable(refreshed);
 	}
 
 	private void readFromParcel(Parcel in) {
-		mProvider = in.readString();
-		mRating = in.readString();
-		mName = in.readString();
-		mId = in.readString();
-		mClubs = in.createStringArray();
-		mState = in.readString();
-		mCountry = in.readString();
-		mLastPlayed = in.readString();
-		mExpires = in.readString();
+		key = in.readLong();
+		provider = in.readString();
+		id = in.readString();
+		rating = in.readString();
+		name = in.readString();
+		clubs = in.createStringArray();
+		state = in.readString();
+		country = in.readString();
+		lastPlayed = in.readString();
+		expires = in.readString();
+		refreshed = (Date) in.readSerializable();
 	}
 
 	@Override
 	public String toString() {
-		return String.format("%s (%s)", mName, mRating);
+		return String.format("%s (%s)", name, rating);
 	}
 
 	public String toDetailedString() {
-		String clubs;
+		String clubsString;
 
-		if (mClubs == null)
-			clubs = null;
+		if (clubs == null)
+			clubsString = null;
 		else {
 			StringBuilder sb = new StringBuilder();
-			for (String club : mClubs) {
+			for (String club : clubs) {
 				if (sb.length() != 0)
 					sb.append(", ");
 				sb.append(club);
 			}
-			clubs = sb.toString();
+			clubsString = sb.toString();
 		}
 
-		return String.format("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t", mId, mExpires,
-				mName, mRating, clubs, mState, mCountry, mLastPlayed);
-	}
-
-	public String getProvider() {
-		return mProvider;
-	}
-
-	public void setProvider(String provider) {
-		this.mProvider = provider;
-	}
-
-	public String getRating() {
-		return mRating;
+		return String.format("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t", id, expires,
+				name, rating, clubsString, state, country, lastPlayed);
 	}
 
 	public String getBaseRating() {
-		if ("usatt".equals(mProvider))
+		if ("usatt".equals(provider))
 			return getRating();
-		else if ("rc".equals(mProvider)) {
+		else if ("rc".equals(provider)) {
 			try {
-				//get up to the +- symbol
-				return getRating().substring(0, getRating().indexOf(177)); 
+				// get up to the +- symbol
+				return getRating().substring(0, getRating().indexOf(177));
 			} catch (Exception ex) {
 			}
 		}
 		return null;
 	}
 
-	public void setRating(String rating) {
-		this.mRating = rating;
+	public Long getKey() {
+		return key;
 	}
 
-	public String getName() {
-		return mName;
+	public void setKey(Long key) {
+		this.key = key;
 	}
 
-	public void setName(String name) {
-		this.mName = name;
+	public String getProvider() {
+		return provider;
+	}
+
+	public void setProvider(String provider) {
+		this.provider = provider;
 	}
 
 	public String getId() {
-		return mId;
+		return id;
 	}
 
 	public void setId(String id) {
-		this.mId = id;
+		this.id = id;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String[] getClubs() {
-		return mClubs;
+		return clubs;
 	}
 
 	public void setClubs(String[] clubs) {
-		this.mClubs = clubs;
+		this.clubs = clubs;
 	}
 
 	public String getState() {
-		return mState;
+		return state;
 	}
 
 	public void setState(String state) {
-		this.mState = state;
+		this.state = state;
 	}
 
 	public String getCountry() {
-		return mCountry;
+		return country;
 	}
 
 	public void setCountry(String country) {
-		this.mCountry = country;
+		this.country = country;
 	}
 
 	public String getLastPlayed() {
-		return mLastPlayed;
+		return lastPlayed;
 	}
 
 	public void setLastPlayed(String lastPlayed) {
-		this.mLastPlayed = lastPlayed;
+		this.lastPlayed = lastPlayed;
 	}
 
 	public String getExpires() {
-		return mExpires;
+		return expires;
 	}
 
 	public void setExpires(String expires) {
-		this.mExpires = expires;
+		this.expires = expires;
+	}
+
+	public Date getRefreshed() {
+		return refreshed;
+	}
+
+	public void setRefreshed(Date refreshed) {
+		this.refreshed = refreshed;
+	}
+
+	public static Parcelable.Creator<PlayerModel> getCreator() {
+		return CREATOR;
 	}
 
 	public static final Parcelable.Creator<PlayerModel> CREATOR = new Parcelable.Creator<PlayerModel>() {
